72ae82a1f6dc2633c690855893c069030f481374
Rjchi <richardospina18@gmail.com>
2023-09-10 16:33:06 -0500

    [Feat] Agrega enpoint para ver los detalles de un anime
    Este commit agrega un nuevo enpoint que nos permite visualizar los detalles de un anime en especifico como: genero, descripcion, nombre etc.

--- controllers/animes.controllers.js
+++ controllers/animes.controllers.js
@@ -4,6 +4,9 @@ const slug = require("slug");
 const { load } = require("cheerio");
 const { BASE_URL_2 } = require("../config.js");
 
+/**-----------------------------------------------
+ * |  Aqui obtenemos los animes por pagina
+ -----------------------------------------------*/
 const getAnimes = async (req, res) => {
   try {
     const page = req.params.page ? req.params.page : "1";
@@ -38,6 +41,9 @@ const getAnimes = async (req, res) => {
   }
 };
 
+/**-----------------------------------------------
+ * |  Aqui obtenemos los enlaces de reproducción
+ -----------------------------------------------*/
 const seeAnime = async (req, res) => {
   try {
     const anime = slug(req.params.anime, "-");
@@ -48,11 +54,11 @@ const seeAnime = async (req, res) => {
         anime === "go-toubun-no-hanayome" ? "go-toubun-no-hanayome∽" : anime
       }-episodio-${episode}-sub-espanol`
     );
-    const $2 = load(data);
+    const $ = load(data);
 
     let links = [];
-    $2("div.mobius select option").each((i, el) => {
-      const htmlString = Buffer.from($2(el).attr("value"), "base64").toString(
+    $("div.mobius select option").each((i, el) => {
+      const htmlString = Buffer.from($(el).attr("value"), "base64").toString(
         "utf-8"
       );
       const match = htmlString.match(/src="([^"]+)"/);
@@ -72,7 +78,53 @@ const seeAnime = async (req, res) => {
   }
 };
 
+/**-----------------------------------------------
+ * |  Aqui obtenemos los detalles de un anime
+ -----------------------------------------------*/
+const animeDetails = async (req, res) => {
+  try {
+    const anime = slug(req.params.anime, "-");
+
+    const { data } = await axios.get(`${BASE_URL_2}anime/${anime}`);
+
+    const $ = load(data);
+    let details = [];
+
+    $("div.infox").each((i, el) => {
+      const genres = [];
+      const information = []
+      $(el)
+        .find("div.ninfo > div.info-content > div.genxed > a")
+        .each((item, genre) => {
+          genres.push($(genre).text());
+        });
+
+      $(el)
+        .find('div.ninfo > div.info-content > div.spe > span')
+        .each((items, info) => {
+          information.push($(info).text());
+        });
+
+      details.push({
+        name: $(el).find("h1").text(),
+        genres: genres,
+        information: information,
+        description: $(el).find("div.ninfo > div.info-content > div.entry-content > p").first().text(),
+      });
+    });
+
+    if (details.length !== 0) {
+      return res.status(200).json(details);
+    } else {
+      return res.status(404).json({ message: `No hay detalles para ${anime}` });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getAnimes,
   seeAnime,
+  animeDetails,
 };
--- routes/animes.routes.js
+++ routes/animes.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
-const { getAnimes, seeAnime } = require("../controllers/animes.controllers.js");
+const {
+  getAnimes,
+  seeAnime,
+  animeDetails,
+} = require("../controllers/animes.controllers.js");
 
 const router = Router();
 
-router.get("/api/animes/:page", getAnimes)
-router.get("/api/see/:anime/:episode", seeAnime)
+router.get("/api/animes/:page", getAnimes);
+router.get("/api/see/:anime/:episode", seeAnime);
+router.get("/api/details/:anime", animeDetails);
 
 module.exports = router;
